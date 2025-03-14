47a741f4a6e7666420163111925bab47f8fa8245
Olgica Kalceva <kalcevaaolgica@gmail.com>
2025-03-14 14:20:03 +0100

    Car-Rasing-Homework

---
+++ Car-Rasing-Homework/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Rasing_Homework
+{
+   public class Car
+    {
+        public string Model { get; set; }
+        public double Speed { get; set; }
+        public Driver Driver { get; set; }
+
+        public Car(string model,double speed)
+        {
+            Model = model;
+            Speed = speed;
+            Driver = null;
+        }
+
+        // Make a method RaceCars() that will get two
+        // Car objects that will determine which car
+        // will win and print the result in the console.
+        // Make 4 car objects and 4 driver objects.
+
+        public double CalculateSpeed()
+        {
+            if (Driver == null)
+            {
+
+                Console.WriteLine("There is no driver");
+                return 0;
+            }
+            return Speed * Driver.Skill;
+        }
+
+
+        public void RaceCars(Car car1, Car car2)
+        {
+            int speed1 = car1.CalculateSpeed(); 
+            int speed2 = car2.CalculateSpeed(); 
+
+            if (speed1 > speed2)
+            {
+                Console.WriteLine($"{car1.Model} was faster.");
+            }
+            else if (speed2 > speed1)
+            {
+                Console.WriteLine($"{car2.Model} was faster.");
+            }
+            else
+            {
+                Console.WriteLine("Something is invalid.");
+            }
+
+
+        }
+
+    }
+}
---
+++ Car-Rasing-Homework/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Car_Rasing_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two cars and two drivers: ");
+
+            Car[] cars = {
+            new Car("Golf", 200),
+            new Car("Mazda", 220),
+            new Car("Ferrari", 300),
+            new Car("Fiat", 280)
+        };
+
+            Driver[] drivers = {
+            new Driver("Bob", 3),
+            new Driver("Teo", 4),
+            new Driver("Ana", 5),
+            new Driver("Sofia", 2)
+
+
+        };
+
+
+        }
+    }
+}
